c78326cd64d0f8a2b4516bb15b6170a1d80c26e8
yikzin <79579710+Yikzin@users.noreply.github.com>
2021-03-22 16:34:38 +0100

    La til ny writefile64 som kan skrive med mer margin

--- writefile.c
+++ writefile.c
@@ -1,14 +1,15 @@
 #include <fcntl.h>
 
 int main(){
-	
-	int fd, sz;
 
-	fd = open("bar.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if(fd < 0) {
-	perror("feil ved skriving av filen"); exit(1);
-	}
-	sz = write(fd, "Hello\0", 6);
-	
-	close(fd);
+        int fd, sz;
+
+        fd = open("bar.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+        if(fd < 0) {
+        perror("feil ved skriving av filen"); exit(1);
+        }
+        sz = write(fd, "Det er bare 6 bytes ledig, dette er ikke lesbart eller skrivbart.",6);
+        close(fd);
 }
+
+
---
+++ writefile64.c
@@ -0,0 +1,14 @@
+#include <fcntl.h>
+
+int main(){
+	
+	int fd, sz;
+
+	fd = open("bar.txt", O_WRONLY | O_CREAT | O_TRUNC, 0644);
+	if(fd < 0) {
+	perror("feil ved skriving av filen"); exit(1);
+	}
+	sz = write(fd, "Det er bare 6 bytes ledig, dette er ikke... vent det er nok her!\0", 64);
+	
+	close(fd);
+}
